7877e30d8fbfead43f320cf1470d5c296ae5152f
david.duarte <david.duarte@coppel.com>
2024-05-06 11:34:12 -0600

    Modulo sencillo con funciones

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  hola: function () {
+    console.log("Hola Mundo!");
+  },
+
+  holaPersonalizado: function (nombre) {
+    console.log(`Hola ${nombre}`);
+  },
+
+  holaAlCursoNPM: function () {
+    console.log("Hola al curso NPM!");
+  },
+};
